9263b6964c5ebed4790b59ce081bd46f3f6d5b18
amgad-shaaban <magoodmessi369@gmail.com>
2026-03-05 02:38:22 +0200

    B:style fix

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-14 bg-black text-white border-b border-zinc-800">
-      <div className="max-w-[1200px] mx-auto h-full flex items-center px-4">
+    <nav className="w-full h-12 bg-black text-white border-b border-zinc-800">
+      <div className="max-w-[1200px] mx-auto h-full flex items-center justify-between px-6">
 
-        {/* LEFT */}
+        {/* LEFT SECTION */}
         <div className="flex items-center gap-6">
           <Link to="/" className="text-orange-500">
             <SiSoundcloud size={28} />
@@ -25,23 +25,22 @@ export default function Navbar() {
         </div>
 
         {/* SEARCH */}
-        <div className="mx-10 w-[400px] relative">
+        <div className="relative w-[420px]">
           <input
             type="text"
             placeholder="Search"
             className="w-full bg-zinc-900 text-sm text-white placeholder-zinc-400
-                       pl-4 pr-10 py-2 rounded-md focus:outline-none"
+                       pl-4 pr-10 py-1.5 rounded-md focus:outline-none"
           />
 
-          {/* Icon on RIGHT */}
           <Search
             size={18}
             className="absolute right-3 top-1/2 -translate-y-1/2 text-zinc-400"
           />
         </div>
 
-        {/* RIGHT */}
-        <div className="flex items-center gap-5 text-sm ml-auto">
+        {/* RIGHT SECTION */}
+        <div className="flex items-center gap-5 text-sm">
           <Link
             to="/pro"
             className="text-orange-500 hover:text-orange-400 font-medium"
@@ -61,7 +60,7 @@ export default function Navbar() {
           <Mail size={18} className="text-zinc-400 hover:text-white cursor-pointer" />
           <MoreHorizontal size={18} className="text-zinc-400 hover:text-white cursor-pointer" />
 
-          <div className="w-8 h-8 bg-zinc-600 rounded-full cursor-pointer" />
+          <div className="w-7 h-7 bg-zinc-600 rounded-full cursor-pointer" />
         </div>
 
       </div>
