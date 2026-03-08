4e2b3d824ac82e89d36de7edaa266f03aef939b6
NadaSerag-github <nadaserag2006@gmail.com>
2026-03-09 00:45:40 +0200

    [setup] add global API configuration with mock/real backend switching

---
+++ src/features/feed/feedservice.ts
@@ -0,0 +1,5 @@
+//import { api } from '@/services/api'; //uncomment this when you have an endpoint to add
+
+export const feedService = {
+  //add endpoints here
+};
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,6 @@
-// AXIOS BASE INSTANCE
+import axios from 'axios';
+import { BASE_URL } from '../config/env';
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+});
