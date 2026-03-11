d5589cac341aaf77785ef6a6fa39aa478f952a11
Asmahan <asmahanibrahimalbitar@gmail.com>
2026-03-11 21:19:06 +0200

    feat(auth): add email format validation and clear form state on route change

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,16 @@
-import SignInPage from './features/auth/pages/SignInPage';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import SignInPage from '../features/auth/pages/SignInPage';
 
-// Inside your routes, add:
-{ path: '/signin', element: <SignInPage /> }
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="*" element={<Navigate to="/signin" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
