12f15f287633745fe4ab2fd9562708538b33c88d
Asmahan <asmahanibrahimalbitar@gmail.com>
2026-03-11 21:18:43 +0200

    feat(auth): register signin and signup routes with state reset on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import NavBar from "./components/layout/Navbar"
-import { createBrowserRouter , RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import SideBar from "./components/layout/Sidebar"
+import SignInPage from "./features/auth/pages/SignInPage"
+import SignUpPage from "./features/auth/pages/SignUpPage"
 
-const router = createBrowserRouter([{
-  path:'/',
-  element:<NavBar/>,
-  children:[{
-    path:'/',
-    element:<SideBar/>
-  }]
-}])
+const router = createBrowserRouter([
+  {
+    path: '/signin',
+    element: <SignInPage key={Math.random()} />,
+  },
+   {
+    path: '/create-account',
+    element: <SignUpPage key={Math.random()} />,
+  },
+  {
+    path: '/',
+    element: <NavBar />,
+    children: [
+      {
+        path: '/',
+        element: <SideBar />
+      }
+    ]
+  }
+])
+
 function App() {
-
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
   )
 }
