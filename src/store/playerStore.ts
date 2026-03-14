df3cda99ecc3616ed1b66e1d111fa366c37af9f8
amgad-shaaban <magoodmessi369@gmail.com>
2026-03-15 01:35:04 +0200

    feat(upload):audiosourceslice

--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -1,1 +1,12 @@
-//
+
+import { configureStore } from "@reduxjs/toolkit"
+import audioSourceReducer from "./AudioSourceSlice.ts"
+
+export const store = configureStore({
+  reducer: {
+    audioSource: audioSourceReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
