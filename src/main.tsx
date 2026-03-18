4ea2121b4cac23a20b2ef65a2029fd08069bbcc0
Asmahan <asmahanibrahimalbitar@gmail.com>
2026-03-18 14:08:27 +0200

    feat(auth): wrap app with GoogleOAuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
-    </Provider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>    </Provider>
   </StrictMode>,
 )
