bbf0af7b70f3a97216150b79eb03698278ed7917
Asmahan <asmahanibrahimalbitar@gmail.com>
2026-03-24 09:25:23 +0200

    fix(main): update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
-// import { setupMockApi } from './features/engagement/data/mockApi';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-// setupMockApi();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
-    </GoogleOAuthProvider>    </Provider>
+    </GoogleOAuthProvider>    
+    </Provider>
   </StrictMode>,
 )
